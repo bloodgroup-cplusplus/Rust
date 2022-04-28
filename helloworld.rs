572c27930509141d51d70a4dc587e8ee368ef3aa
Bhushan <tucater10896601@protonmail.com>
2022-04-28 09:30:56 +0530

    added second way of printing hello world

--- helloworld.rs
+++ helloworld.rs
@@ -2,6 +2,9 @@ fn main()
 {
   println!("Hello World");
   
+  // instaed of using a single literal string, you can print several of them, even in a single
+  // statement. In this way .
+  print!("{},{}!", "Hello","World");
   
  }
  
