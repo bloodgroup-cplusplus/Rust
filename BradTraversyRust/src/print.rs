b989f96d2856e11706831b104523da29a9d69e8f
Bhushan <tucater10896601@protonmail.com>
2022-04-29 12:09:43 +0530

    added placholder traits, placeholder arguments, placeholder for debug trait and basic math

--- BradTraversyRust/src/print.rs
+++ BradTraversyRust/src/print.rs
@@ -6,6 +6,33 @@ pub fn run()
     // Print to console 
 
     println!("Hello From the print.rs File");
+    println!("Number :{}",1);
+    // add multiple place holders 
+    println!("{} is from {}", "Brad", "Mass");
+
+    //Positional Arguemnts
+
+    println!("{0} is from {1} and {0} likes to {2}", "Brad", "Mass", "code");
+
+    //Named Arguments 
+    
+    println!("{name} likes to play {activity}", name = "John", activity = "Football");
+
+
+    //Placeholder traits 
+
+    println!("Binary: {:b} Hex:{:x} Octal :{:o}", 10,10,10);
+
+
+
+    // Placeholder for debug traits 
+
+    println!("{:?}", (12,true,"hello"));
+
+    // Basic Math 
+    
+    println!("10 + 10 = {}", 10+10);
+
     }
 
 
