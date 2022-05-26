9894d7c58c563028769cd7a37f3396b8f3aac3b9
Bhushan <tucater10896601@protonmail.com>
2022-05-26 14:50:29 +0530

    added target

---
+++ catsy/target/debug/catsy.d
@@ -0,0 +1,1 @@
+/Users/bhushansharma/Programs/Rust/catsy/target/debug/catsy: /Users/bhushansharma/Programs/Rust/catsy/src/main.rs
---
+++ catsy/target/debug/deps/catsy-94353de5267c99f9.d
@@ -0,0 +1,5 @@
+/Users/bhushansharma/Programs/Rust/catsy/target/debug/deps/catsy-94353de5267c99f9: src/main.rs
+
+/Users/bhushansharma/Programs/Rust/catsy/target/debug/deps/catsy-94353de5267c99f9.d: src/main.rs
+
+src/main.rs:
