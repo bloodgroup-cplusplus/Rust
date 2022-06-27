0826655f23ed4bb3678362dbd3616066c25e83ae
Bhushan <tucater10896601@protonmail.com>
2022-06-27 15:57:22 +0530

    added code in main.rs

--- RustFrontEnd/RustFrontEnd/src/main.rs
+++ RustFrontEnd/RustFrontEnd/src/main.rs
@@ -1,3 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+use yew::prelude::*;
+
+//create model for web app
+
+struct Model
+{
+    value:i64
+}
+
+#[function_component(App)]
+fn app() ->Html
+{
+    let state = use_state(|| Model{
+    value:0});
+
+    let onclick = {
+        let state = state.clone();
+        
+        Callback::from(move |_|{
+            state.set(Model{
+            value:state.value+1
+        })
+    }
+}
+
+    html!{
+        <div>
+            <button onclick = {onclick}->{"+1"}</button>
+            <p> { state.value}</p>
+            </div>
+    }
+    
+}
+fn main()
+{
+    yew::start_app::<App>();
 }
